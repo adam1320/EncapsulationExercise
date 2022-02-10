2154fff62f7e58495e5e4397a4af2e47650da49f
Adam Brannon <adam1320@rocketmail.com>
2022-02-10 13:18:19 -0500

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit (double depAmount)
+        {
+            Console.WriteLine($"Deposting {depAmount, 0:C} into your account...");
+
+            _balance = _balance += depAmount;
+        }
+
+        public double GetBalance ()
+        {
+
+            return _balance;
+        }
+
+       
+        
+        
+
+        
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount MyAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit into your account today?");
+            double depositAmount = (double.Parse(Console.ReadLine()));
+            
+            MyAccount.Deposit(depositAmount);
+            double currentBalance = MyAccount.GetBalance();
+            
+            Console.WriteLine($"Your balance is {currentBalance, 0:C}");
+        }
+    }
+}
